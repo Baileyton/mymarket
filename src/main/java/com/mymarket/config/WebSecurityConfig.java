772758c57cc0d6617096e57e1e086b4c613c6433
Baileyton <baileyton@naver.com>
2024-07-03 14:22:19 +0900

    refactor : 상품 조회 요청 허가

--- src/main/java/com/mymarket/config/WebSecurityConfig.java
+++ src/main/java/com/mymarket/config/WebSecurityConfig.java
@@ -61,7 +61,9 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/user/login").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/products").permitAll()
+                        .requestMatchers("/api/product/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
